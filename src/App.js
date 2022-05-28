c2c326af842ada107d81838316296d313f20781d
hargun0360 <hargunsingh0720@gmail.com>
2022-05-28 18:38:13 +0530

    Add Reset Password Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SignUp from './components/auth/SignUp/SignUp';
 import Login from './components/auth/Login/Login';
 import Otp from './components/auth/OTP/OTP';
 import Forgot from './components/auth/ForgotPass/ForgotPass';
+import ResetPass from './components/auth/ForgotPass/ResetPass';
 function App() {
   return ( <>
        <Routes>
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/otp" element={<Otp />} />
         <Route exact path="/forgot" element={<Forgot />} />
+        <Route exact path="/reset" element={<ResetPass />} />
       </Routes>
     </>
   );
